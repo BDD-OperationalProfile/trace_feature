1d1978cb6356bff11d0b9c2895883c2b4a588905
Felipe Hargreaves <fhargreaves00@gmail.com>
2018-06-09 21:39:58 -0300

    Creates project's models
    Co-authored-by: Bernardo Henrique <bernardohrl.unb@gmail.com>
    Co-authored-by: Bruna Pinos <bruna.pinos@gmail.com>
    Co-authored-by: Clarissa Borges <clarissalimab08@gmail.com>
    Co-authored-by: Daniel Teles <danieelteles@gmail.com>
    Co-authored-by: Felipe Hargreaves <fhargreaves00@gmail.com>
    Co-authored-by: Halê Valente Silva <halevalente@gmail.com>
    Co-authored-by: Leticia de Souza <leticia.souza212@gmail.com>
    Co-authored-by: Mariana Pícolo <marianapicolo@hotmail.com>

---
+++ trace_feature/models/scenario_outline.py
@@ -0,0 +1,25 @@
+import Scenario
+
+class ScenarioOutline(Scenario):
+
+    def __init__(self):
+        self.steps = []
+        self.scenario_title = ""
+        self.line = None
+        self.examples = []
+        self.scenario_iterations = []
+
+    def execute(self):
+        pass
+
+    def set_line(self):
+        pass
+
+    def add(self):
+        pass
+
+    def remove(self):
+        pass
+
+    def execute(self):
+        pass
