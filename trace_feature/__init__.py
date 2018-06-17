c4ec7063a47bf1ef807affaf9917ceef26ccca69
leticiadesouza <leticia.souza212@gmail.com>
2018-06-16 22:58:58 -0300

    Verify requirements

--- trace_feature/__init__.py
+++ trace_feature/__init__.py
@@ -1,5 +1,11 @@
 from .core.ruby.ruby_execution import RubyExecution
+from .core.ruby.ruby_config import RubyConfig
+
 
 def execute():
     ex = RubyExecution()
-    ex.execute_scenario('/home/leticia/Documents/Desenho/trabalho2/myrottenpotatoes/features/adicionar_filme.feature', 10)
+    config = RubyConfig()
+    if config.config():
+        ex.execute_scenario('/home/leticia/Documents/Desenho/trabalho2/myrottenpotatoes/features/adicionar_filme.feature', 10)
+    else:
+        print('Error!!!')    
