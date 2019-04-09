240163153e1b999d86e46dda2a0a0b23b194d1b9
Rafael Fazzolino <fazzolino29@gmail.com>
2019-04-09 19:44:34 -0300

    Incluindo modulo para leitura dos specs

---
+++ trace_feature/core/ruby/spec/ruby_spec_execution.py
@@ -0,0 +1,5 @@
+# Criar métodos para ler estaticamente todos os "its" presentes no projeto analisado.
+
+# Com o array de "its" disponível e todos seus dados preenchidos, podemos executar 1 por 1 e registrar os métodos
+# executados, assim como é feito com o BDD. A diferença é que o BDD executa um cenário, aqui vamos executar 1 it de cada
+# vez e salvar a execução.
---
+++ trace_feature/core/ruby/spec/spec_models.py
@@ -0,0 +1,32 @@
+class Method:
+
+    def __init__(self):
+        self.method_id = ""
+        self.method_name = ""
+        self.class_name = ""
+        self.class_path = ""
+
+    def __str__(self):
+        print("METHOD:")
+        print("\t\t\t name: " + self.method_name)
+        print("\t\t\t classe: " + self.class_name)
+        print("\t\t\t path: " + self.class_path)
+        return ''
+
+
+# Ver a necessidade de utilizar a modelo de Describe tb.. acho que não precisa.
+# class Describe:
+#     def __init__(self):
+#         self.description = ""
+#         self.line = None
+
+
+class It:
+    def __init__(self):
+        self.description = ""
+        self.line = None
+        self.executed_methods = []
+
+    def __str__(self):
+        return self.description
+
